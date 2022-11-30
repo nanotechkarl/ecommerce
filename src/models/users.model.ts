5d7b44d1dbef1378d6b50b9eff22606c52180112
KARL ENANO <karlenano31@gmail.com>
2022-12-01 07:48:17 +0800

    Added users model

---
+++ src/models/users.model.ts
@@ -0,0 +1,35 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Users extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  id: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+
+  constructor(data?: Partial<Users>) {
+    super(data);
+  }
+}
+
+export interface UsersRelations {
+  // describe navigational properties here
+}
+
+export type UsersWithRelations = Users & UsersRelations;
